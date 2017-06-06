33afed88355097b097b1cdf37328c3729ea6d356
vanezp01 <vanezp01@gmail.com>
2017-06-06 11:40:40 -0700

    add show actions and view pages

--- app/controllers/flicks_controller.rb
+++ app/controllers/flicks_controller.rb
@@ -1,6 +1,11 @@
 class FlicksController < ApplicationController
-
+  before_action :find_flick, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @flicks = Flick.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -9,11 +14,21 @@ class FlicksController < ApplicationController
 
   def create
     @flick = Flick.new(flick_params)
+
+    if @flick.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   private
 
   def flick_params
-    params.require(:play).permit(:title, :description, :director)
+    params.require(:flick).permit(:title, :description, :director)
+  end
+
+  def find_flick
+    @flick = Flick.find(params[:id])
   end
 end
