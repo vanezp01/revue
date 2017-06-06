3e3aba7c9683c4e6838431e613925e52e2005ed7
vanezp01 <vanezp01@gmail.com>
2017-06-06 11:12:28 -0700

    add actions and view files

--- app/controllers/flicks_controller.rb
+++ app/controllers/flicks_controller.rb
@@ -2,4 +2,18 @@ class FlicksController < ApplicationController
 
   def index
   end
+
+  def new
+    @flick = Flick.new
+  end
+
+  def create
+    @flick = Flick.new(flick_params)
+  end
+
+  private
+
+  def flick_params
+    params.require(:play).permit(:title, :description, :director)
+  end
 end
