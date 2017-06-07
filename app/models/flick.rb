b1305b2d91f6d15239ddae1a9a30aaccc88fb292
vanezp01 <vanezp01@gmail.com>
2017-06-07 04:24:52 -0700

    render images on index page

--- app/models/flick.rb
+++ app/models/flick.rb
@@ -2,6 +2,6 @@ class Flick < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  has_attached_file :flick_img, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :flick_img, :styles =>  { :flick_index => "300x300>", :flick_show => "325x475>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :flick_img, content_type: /\Aimage\/.*\z/
 end
